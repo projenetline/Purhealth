3f34694e951f5424000e0746cb519acbafa7761b
neizenis <ismail.ozer@netline.net.tr>
2021-11-10 09:40:01 +0300

    Proje Yükleme

---
+++ NetlineLogoWebApi.Tests/Controllers/ClCardControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetlineLogoWebApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetlineLogoWebApi.Controllers.Tests
+{
+    [TestClass()]
+    public class ClCardControllerTests
+    {
+        [TestMethod()]
+        public void GetCurrentTest()
+        {
+            ReportController controller = new ReportController();
+            //controller.GetCurrent(5,null);
+            throw new NotImplementedException();
+        }
+    }
+}
